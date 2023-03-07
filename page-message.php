dd4125b3c2a61f57aac36f41f90e90b060f7de13
Ryo0417 <liangtaijinzi58@gmail.com>
2023-03-07 21:55:07 +0900

    message-0307-01

--- page-message.php
+++ page-message.php
@@ -1,34 +1,108 @@
 <?php get_header(); ?>
-<div class="sub-message__bg-circle">
+<div class="sub-message__bg-circle common-bg-circle">
   <section class="sub-message">
     <div class="sub-message__inner">
       <h1 class="sub-message__head">message</h1>
       <div class="sub-message__reception">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-enterprise.svg" alt="グループエンタープライズ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-enterprise-sp.svg" alt="グループエンタープライズ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-enterprise-sp.svg" alt="グループエンタープライズ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-enterprise-sp.svg" alt="グループエンタープライズ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-enterprise-sp.svg" alt="グループエンタープライズ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-enterprise-sp.svg" alt="グループエンタープライズ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-enterprise-sp.svg" alt="グループエンタープライズ">
       </div>
-      <div class="sub-message__companies">
-        <div class="sub-message__company sub-company">
-          <h2 class="sub-company__name">株式会社TSUBAKI不動産</h2>
-          <div class="sub-company__link">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/arrow-diagonal.svg" alt="">
-            <a href="">official hp</a>
-          </div>
-          <div class="sub-company__img">
-            <picture>
-              <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/message/message1-sp.jpg" media="(max-width: 767px)" />
-              <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/message/message1-pc.jpg" media="(min-width: 768px)" />
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message1-pc.jpg" alt="社長の画像１">
-            </picture>
-            <figure class="sub-company__name-img">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-name1.png" alt="社長の名前">
-            </figure>
-          </div>
-          <h3 class="sub-company__ceo">
-            藤本 律夫<span>株式会社TSUBAKI不動産 代表取締役</span>
-          </h3>
+    </div>
+    <div class="sub-message__companies">
+      <div class="sub-message__company sub-company">
+        <h2 class="sub-company__name">株式会社TSUBAKI不動産</h2>
+        <div class="sub-company__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/arrow-diagonal.svg" alt="">
+          <a href="">official hp</a>
         </div>
-  
+        <div class="sub-company__img">
+          <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/message/message1-sp.jpg" media="(max-width: 767px)" />
+            <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/message/message1-pc.jpg" media="(min-width: 768px)" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message1-pc.jpg" alt="社長の画像１">
+          </picture>
+          <figure class="sub-company__name-img">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-name1.png" alt="社長の名前">
+          </figure>
+        </div>
+        <h3 class="sub-company__ceo">
+          <p>藤本 律夫</p>
+          <span>株式会社TSUBAKI不動産 代表取締役</span>
+        </h3>
+        <p class="sub-company__desc">
+          株式会社TSUBAKI不動産は、令和5年4月1日より｢第17期｣を迎え、ライフグループとして2035年に向けたビジョン、｢共創型コミュニティプラットフォーム｣を制定しました。<br>
+          このビジョンを制定するにあたり、あらゆる業界においてAIなどテクノロジーの革新的な進歩や様々な価値観や社会の変化、また異業種参入などによって、｢必要とされる商品やサービスの変化、また開発スピード｣は劇的に早くなってきているという背景があります。<br>
+          そのような時代において、より多くの方から必要とされる商品やサービスを提供し続けるためには、社内に限らず必要な知識や技術を持つ多くのステークホルダーが集まり、｢新たな商品やサービスが共創という形で作られていく｣という環境を整備することが最も大切だと考えました。<br>
+          そのグループビジョンの達成に向けて、株式会社TSUBAKI不動産が担う役割は、増え続ける空き家問題の解決を第一としています。空き家となった住まいを新たな価値を持つ住宅へ再生、または、新築住宅として生まれ変わらせていくことで、若い世代へ繋げていき、地域活性を図ります。また、高齢者にとって住みやすい賃貸住宅へ再生することも大切な役割として取組みを進めてまいります。
+        </p>
       </div>
+      <div class="sub-message__company sub-company">
+        <h2 class="sub-company__name">行政書士法人ライフ</h2>
+        <div class="sub-company__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/arrow-diagonal.svg" alt="">
+          <a href="">official hp</a>
+        </div>
+        <div class="sub-company__img">
+          <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/message/message2-sp.jpg" media="(max-width: 767px)" />
+            <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/message/message2-pc.jpg" media="(min-width: 768px)" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message2-pc.jpg" alt="社長の画像２">
+          </picture>
+          <figure class="sub-company__name-img">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-name2.png" alt="社長の名前">
+          </figure>
+        </div>
+        <h3 class="sub-company__ceo">
+          <p>永戸 康弘</p>
+          <span>行政書士法人ライフ 代表社員</span>
+        </h3>
+        <p class="sub-company__desc">
+          相続手続き・生前対策の専門家集団として平成29年に行政書士法人ライフを設立しました。ライフグループの中では、高齢者やおひとりさまが抱える問題の解決や、高齢者と不動産を繋ぐ法務的役割を担っています。<br>相続の専門家がお客様のパートナーとして人生の最期まで寄り添うためには、法人化が必要でした。<br>また、より多くのお客様へ最適なご提案をするために、これまで培ったノウハウの蓄積を共有し、家族信託や身元保証などのサービスを積極的に取り入れてきました。<br>令和３年には福山支店を開設、所属する行政書士も増え、広島県全域をカバーできるような体制も整いました。また、現在は、県外に支店を開設する準備を行っております。<br>2035年に向けたグループビジョンを達成するため、また、20年後、30年後もお客様の一生涯のパートナーとしてあり続けるために、行政書士法人ライフは業務に邁進してまいります。
+        </p>
+      </div>
+      <div class="sub-message__company sub-company">
+        <h2 class="sub-company__name">株式会社ライフプラス</h2>
+        <div class="sub-company__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/arrow-diagonal.svg" alt="">
+          <a href="">official hp</a>
+        </div>
+        <div class="sub-company__img">
+          <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/message/message3-sp.jpg" media="(max-width: 767px)" />
+            <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/message/message3-pc.jpg" media="(min-width: 768px)" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message3-pc.jpg" alt="社長の画像３">
+          </picture>
+          <figure class="sub-company__name-img">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/message-name3.png" alt="社長の名前">
+          </figure>
+        </div>
+        <h3 class="sub-company__ceo">
+          <p>藤本 佐登子</p>
+          <span>株式会社ライフプラス 代表取締役</span>
+        </h3>
+        <p class="sub-company__desc">
+          当たり前だった生活が、思うようにできなくなってしまった時、誰もがご家族や周囲の皆様の手助けが必要となることがあります。
+          大きな変化はご本人だけではなく、力を添える皆様の生活も変えてしまう事があります。
+          私たちライフプラスではそんな皆様のお気持ちに寄り添い、お客様が抱える問題の解決を目指し、高い提案力でサポートいたします。
+          今を楽しみ、明日の楽しみを見つけて頂くことが私たちの願いです。
+          これからもその気持ちを忘れることなく、｢私たちには何ができるだろう｣を考えながら、社会から必要とされる会社を目指してまいります。
+        </p>
+      </div>
+    </div>
+  </section>
+  <section class="sub-member">
+    <div class="sub-member__head">
+      <span>//////</span>
+      <h2>our member</h2>
+      <span>//////</span>
+    </div>
+    <div class="sub-member__img">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/message/our-member-text.png" alt="集合写真">
+      <!-- <p>member</p> -->
     </div>
   </section>
 </div>
